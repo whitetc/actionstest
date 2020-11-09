b1ab2190026ec503ea6a9b04f7862b5fa5eb37a7
User <user@Users-Macintosh.attlocal.net>
2020-11-09 10:41:13 -0500

    first commit

---
+++ src/test/java/Mytest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Mytest {
+    @Test
+    public void test(){
+        fail("not yet implemented");
+    }
+}
